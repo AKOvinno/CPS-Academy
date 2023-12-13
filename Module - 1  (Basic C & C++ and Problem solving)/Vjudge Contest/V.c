3775fb62b8ac5df8177efe4a061a6af155c20449
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-12-13 22:23:13 +0600

    added more

--- Module - 1  (Basic C & C++ and Problem solving)/Vjudge Contest/V.c
+++ Module - 1  (Basic C & C++ and Problem solving)/Vjudge Contest/V.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+    int i, j;
     while(1) {
         int h, w;
         scanf("%d %d", &h, &w);
@@ -9,8 +10,28 @@ int main()
         }
         for(i = 1; i <= h; i++) {
             if(i % 2 == 1) {
-                for()
+                for(j = 1; j <= w; j++) {
+                    if(j % 2 == 1) {
+                        printf("#");
+                    }
+                    else if(j % 2 == 0) {
+                        printf(".");
+                    }
+                }
+                printf("\n");
+            }
+            else if(i % 2 == 0) {
+                for(j = 1; j <= w; j++) {
+                    if(j % 2 == 1) {
+                        printf(".");
+                    }
+                    else if(j % 2 == 0) {
+                        printf("#");
+                    }
+                }
+                printf("\n");
             }
         }
+        printf("\n");
     }
 }
